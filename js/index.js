474f2933bf2ede32b60f7ee9c7f4ebc678acb53c
FouadAbdelhalim <121126454+Fouadabdelhalim9@users.noreply.github.com>
2024-10-24 15:52:58 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav-item .nav-link');
+    
+    navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+        navLinks.forEach(item => item.classList.remove('active'));
+        this.classList.add('active');
+      });
+    });
+  });
+
+
+  // slider
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+  
